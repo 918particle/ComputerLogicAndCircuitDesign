465cdaeeed423734936c099b4c8fb5503e443dd8
Jordan <918particle@gmail.com>
2021-09-14 14:00:46 -0700

    Added new reading quiz for unit 1, updated dates and format for unit 1 lecture notes.

---
+++ wavedrom/timingExample14.py
@@ -0,0 +1,24 @@
+import wavedrom
+signals = """
+{
+	"signal":
+		[
+			{ "name": "CLK", "wave": "p..............."},
+			[ "Data",
+				{ "name": "D0", "wave": "lhlhlhlhlhlhlhlh"},
+				{ "name": "D1", "wave": "l.h.l.h.l.h.l.h."},
+				{ "name": "D2", "wave": "l...h...l...h..."},
+				{ "name": "D3", "wave": "l.......h......."}
+			]
+		],
+	"head":
+		{
+			"tock":0
+		},
+	"foot":
+		{
+			"tock":0
+		}
+}"""
+svg = wavedrom.render(signals)
+svg.saveas("timingExample14.svg")
