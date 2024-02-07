5221ca1b464fd37ce213aa5cddf742718591b210
Jordan C hanson <918particle@gmail.com>
2024-02-07 08:11:02 -0800

    added some content, warm up, python code

---
+++ homework_solutions/homework_2/converter.py
@@ -0,0 +1,11 @@
+def bin2dec(x):
+	if(isinstance(x,str)):
+		n = len(x)
+		count=n
+		result=0
+		for i in x:
+			result+=int(i)*(2**(count-1))
+			count-=1
+		return result
+	else:
+		print("Input must be a string.")
